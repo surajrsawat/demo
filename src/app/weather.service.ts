cbeb0e51bbea27a7063859324672cea1f60aa32c
suraj <surajsawat1@gmail.com>
2019-08-13 10:44:43 +0530

    final commit

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,7 +9,7 @@ export class WeatherService {
   constructor(private _http: HttpClient) { }
 
   dailyForecast() {
-    return this._http.get("http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
+    return this._http.get("https://api.myjson.com/bins/ivrkq")
       .map(result => result);
   }
 
